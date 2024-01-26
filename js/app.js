c91526b6c6cf2d9689a0621df0f7ad631aeae639
Arefin Nahid <nahid7ar@gmail.com>
2024-01-26 17:50:36 +0600

    contact and downlord button work

--- js/app.js
+++ js/app.js
@@ -1,22 +1,61 @@
 $(document).ready(function(){
     $('.slider').slick({
-        arrows:false,
-        dots:true,
-        appendDots:'.slider-dots',
-        dotsClass:'dots'
+        arrows: false,
+        dots: true,
+        appendDots: '.slider-dots',
+        dotsClass: 'dots'
     });
-    
-    
+
     let hamberger = document.querySelector('.hamberger');
     let times = document.querySelector('.times');
     let mobileNav = document.querySelector('.mobile-nav');
-    
+
     hamberger.addEventListener('click', function(){
-      mobileNav.classList.add('open');  
+        mobileNav.classList.add('open');  
     });
-    
+
     times.addEventListener('click', function(){
         mobileNav.classList.remove('open');  
     });
-    
+
+    const mobileNavLinks = document.querySelectorAll('.mobile-nav ul li a');
+    mobileNavLinks.forEach(link => {
+        link.addEventListener('click', function() {
+            mobileNav.classList.remove('open');
+        });
     });
+
+
+
+    document.getElementById('contactButton').addEventListener('click', function() {
+        document.getElementById('contact').scrollIntoView({ behavior: 'smooth', block: 'start' });
+        if (mobileNav.classList.contains('open')) {
+            mobileNav.classList.remove('open');
+        }
+    });
+    
+    const contactForm = document.querySelector('.contact form');
+    contactForm.addEventListener('submit', function(event) {
+        event.preventDefault();
+    });
+
+    document.getElementById('downloadCvButton').addEventListener('click', function() {
+        const pdfUrl = './js/cv.pdf';
+        const downloadLink = document.createElement('a');
+        downloadLink.href = pdfUrl;
+        downloadLink.download = 'MdArefinMollaResume.pdf';
+        document.body.appendChild(downloadLink);
+        downloadLink.click();
+        document.body.removeChild(downloadLink);
+    });
+
+    $('#downloadCvButtonFreelancer').click(function() {
+        const pdfUrl = './js/cv.pdf';
+        const downloadLink = document.createElement('a');
+        downloadLink.href = pdfUrl;
+        downloadLink.download = 'MdArefinMollaResume.pdf';
+        document.body.appendChild(downloadLink);
+        downloadLink.click();
+        document.body.removeChild(downloadLink);
+    });
+});
